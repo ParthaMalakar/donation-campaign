5af6cc6939855d306b7dce175a6233281db21864
ParthaMalakar <parthamalakar4321@gmail.com>
2023-09-25 04:47:32 +0600

    card section color add for title ,category bacground and card bacground colour ,here each category have same colour but different colour for other categorys

--- src/Components/Category/Category.jsx
+++ src/Components/Category/Category.jsx
@@ -1,16 +1,29 @@
 
 
 const Category = ({ category }) => {
+    const style ={
+        backgroundColor:`${category.card_bg_color}`
+    }
+    const textColour ={
+        color:`${category.text_button_bg_color}`,
+        border:'1px solid red',
+        padding:'4px',
+        backgroundColor:`${category.category_bg_color}`
+
+    }
+    const text ={
+        color:`${category.text_button_bg_color}`
+    }
     return (
         <div>
 
-            <div className="card  bg-base-100 shadow-xl">
+            <div className="card  bg-base-100 shadow-xl" style={style}>
                 <figure><img className="w-[312px] h-[194px]" src={category.picture} alt="Shoes" /></figure>
                 <div className="card-body">
-                    <h2 className="card-title">
+                    <h2 style={textColour} className="card-title font-medium text-base w-24">
                         {category.category}
                     </h2>
-                    <p>{category.title}</p>
+                    <p style={text} className="font-semibold text-lg">{category.title}</p>
                 </div>
             </div>
         </div>

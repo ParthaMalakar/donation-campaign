3613c071b2cc30203bada682662aaf9fb9158e41
ParthaMalakar <parthamalakar4321@gmail.com>
2023-09-26 01:24:28 +0600

    error page design and route added

--- src/Components/ErrorPage/ErrorPage.jsx
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -1,8 +1,10 @@
 
 const ErrorPage = () => {
     return (
-        <div>
-            <h3>OPPS</h3>
+        <div className="mt-20 max-w-lg mx-auto text-white border-solid border-2 border-indigo-600 bg-slate-500">
+            <h3>OPPS </h3>
+            <p>Some Things went worng</p>
+            <p>Check the internet connection and Route</p>
         </div>
     );
 };
